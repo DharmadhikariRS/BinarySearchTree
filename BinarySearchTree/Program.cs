a66f7fb0c38e2fb616f218e77384b1b0de21c411
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-08-29 18:02:33 +0530

    [Rushikesh] Add node in binary search tree

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,6 +6,7 @@
         {
             BinarySearchTreeOperations<int> binarySearchTreeOperations = new BinarySearchTreeOperations<int>();
             Console.WriteLine("Enter 1 to Create Binary Search Tree");
+            Console.WriteLine("Enter 2 to Add elements in Binary Search Tree");
 
             int Value = int.Parse(Console.ReadLine());
 
@@ -15,6 +16,21 @@
                     binarySearchTreeOperations.AddElement(56);
                     binarySearchTreeOperations.AddElement(30);
                     binarySearchTreeOperations.AddElement(70);
+                    break;
+                case 2:
+                    binarySearchTreeOperations.AddElement(56);
+                    binarySearchTreeOperations.AddElement(30);
+                    binarySearchTreeOperations.AddElement(22);
+                    binarySearchTreeOperations.AddElement(40);
+                    binarySearchTreeOperations.AddElement(11);
+                    binarySearchTreeOperations.AddElement(3);
+                    binarySearchTreeOperations.AddElement(16);
+                    binarySearchTreeOperations.AddElement(70);
+                    binarySearchTreeOperations.AddElement(60);
+                    binarySearchTreeOperations.AddElement(95);
+                    binarySearchTreeOperations.AddElement(65);
+                    binarySearchTreeOperations.AddElement(63);
+                    binarySearchTreeOperations.AddElement(67);
                     break;
                 default:
                     Console.WriteLine("Enter valid input");
